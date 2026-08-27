76a312364de776e99eee7504803a044289754ae5
Fernando Barrera <barrerafernando54@gmail.com>
2026-08-26 23:44:58 -0400

    Changes

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
 })
 export class App {
-  protected readonly title = signal('E-Commerce');
+  protected readonly title = signal('Ropa y Accesorios');
 }
